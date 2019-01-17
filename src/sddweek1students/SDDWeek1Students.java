585914f5dbaaf48e3de2c7c4e7b99239a3a6dfee
koehlers2 <koehlers@shernet.sheridancollege.ca>
2019-01-17 12:59:44 -0500

    Initial Commit

---
+++ src/sddweek1students/SDDWeek1Students.java
@@ -0,0 +1,41 @@
+/*
+    Name:  Sebastian Koehler
+    Assignment:  [assignment name]
+    Program: PROG24178
+    Date:  [assignment due date here]
+
+    Description:
+    [program description in your own words]
+*/
+/*
+ * Sebastian Koehler
+ * Student ID: 991 480 670
+ * SYST10199 - Web Programming
+ */
+
+package sddweek1students;
+
+/**
+ *
+ * @author Seby
+ */
+public class SDDWeek1Students {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student s1 = new Student("Sebastian");
+        Student[] studentList = new Student[3];
+        studentList[0] = s1;
+        studentList[1] = new Student("amrit kaur");
+        studentList[2] = new Student("Malav Jani");
+        
+        for (int i = 0; i < studentList.length;i++){
+            System.out.println(studentList[0].getName());
+        }
+        
+    }
+
+}
---
+++ src/sddweek1students/Student.java
@@ -0,0 +1,37 @@
+/*
+    Name:  Sebastian Koehler
+    Assignment:  [assignment name]
+    Program: PROG24178
+    Date:  [assignment due date here]
+
+    Description:
+    [program description in your own words]
+*/
+/*
+ * Sebastian Koehler
+ * Student ID: 991 480 670
+ * SYST10199 - Web Programming
+ */
+
+package sddweek1students;
+
+/**
+ *
+ * @author Seby
+ */
+public class Student {
+    private String name;
+    
+    //Constructors
+    Student(String name){
+        setName(name);
+    }
+    //Accessors/Getters
+    public String getName(){
+        return this.name;
+    }
+    //Mutators/Setters
+    public void setName(String name){
+        this.name=name;
+    }
+}
